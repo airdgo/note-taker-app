b4e679d5c4a6ffa5c1935a529903642455150c3b
airdgo <vladdragoi1@gmail.com>
2021-09-21 14:52:35 +0300

    added modal when clicking view more button

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const input = document.getElementById('input');
 const submitNote = document.getElementById('submit');
 const noteWraper = document.getElementById('note-wraper');
 
+const openButton = document.getElementById('open');
+const closeButton = document.getElementById('close');
+const modalContainer = document.getElementById('modal_container');
+const modal = document.getElementById('modal');
+const modalText = document.getElementById('modal_text');
+
 submitNote.addEventListener('click', e => {
     e.preventDefault();
     submitNoteFunction();
@@ -13,20 +19,27 @@ const submitNoteFunction = () => {
     const h3 = document.createElement('h3');
     h3.innerText = 'Note:';
     const p = document.createElement('p');
-    p.innerText = input.value;
-    const truncatedInput = truncate(p.innerText);
+    p.innerText = truncate(input.value);
+    const ps = document.createElement('p');
+    ps.innerText = input.value;
 
-    console.log(truncatedInput);
     const viewMoreButton = document.createElement('button');
     viewMoreButton.innerText = 'View More';
+    viewMoreButton.onclick = () => {
+        modalText.innerText = ps.innerText;
+        modalContainer.classList.add('show');
+    }
 
     noteOutput.appendChild(h3);
-    noteOutput.append(truncatedInput);
+    noteOutput.appendChild(p);
     noteOutput.appendChild(viewMoreButton);
-
     noteWraper.appendChild(noteOutput);
-
 }
+
+closeButton.addEventListener('click', e => {
+    e.preventDefault();
+    modalContainer.classList.remove('show');
+});
 
 function truncate(input) {
     if (input.length > 150) {
