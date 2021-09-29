2c41ecae8c061e0f8edb437ab7b2e3f5fb44e72a
airdgo <vladdragoi1@gmail.com>
2021-09-29 16:38:19 +0300

    fixed a bug where add expenses would not display

--- script.js
+++ script.js
@@ -1,53 +1,53 @@
-const addNoteButton = document.getElementById('submit');
-const input = document.getElementById('input');
-const clearNotes = document.getElementById('clear_notes');
-const modalContainer = document.getElementById('modal_container');
-const closeButton = document.getElementById('close');
+const addNoteButton = document.getElementById("submit");
+const input = document.getElementById("input");
+const clearNotes = document.getElementById("clear_notes");
+const modalContainer = document.getElementById("modal_container");
+const closeButton = document.getElementById("close");
 
 // Add notes
-addNoteButton.addEventListener('click', e => {
-    e.preventDefault();
+addNoteButton.addEventListener("click", (e) => {
+	e.preventDefault();
 
-    if (input.value == '') {
-        return alert('Please add a note')
-    }
+	if (input.value == "") {
+		return alert("Please add a note");
+	}
 
-    let notes = localStorage.getItem('notes');
-    if (notes === null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    };
+	let notes = localStorage.getItem("notes");
+	if (notes === null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
 
-    let myObj = {
-        input: input.value
-    };
+	let myObj = {
+		input: input.value,
+	};
 
-    notesObj.push(myObj);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    input.value = '';
+	notesObj.push(myObj);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	input.value = "";
 
-    showNotes();
-})
+	showNotes();
+});
 
 // Clear all notes
 clearNotes.onclick = () => {
-    localStorage.clear();
+	localStorage.clear();
 };
 
 // Show notes on page
 const showNotes = () => {
-    let notes = localStorage.getItem('notes');
-    let noteWraper = document.getElementById('note-wraper');
-    if (notes === null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    };
+	let notes = localStorage.getItem("notes");
+	let noteWraper = document.getElementById("note-wraper");
+	if (notes === null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
 
-    let html = ''
-    notesObj.forEach((element, index) => {
-        html += `
+	let html = "";
+	notesObj.forEach((element, index) => {
+		html += `
                 <div class="note-output">
                     <div class="note-head">
                         <h3>Note</h3>
@@ -56,54 +56,54 @@ const showNotes = () => {
                     <p>${truncated(element.input)}</p>
                     <button id="${index}" class="view-more-button" onclick="viewMore(this.id)">View More</button> 
                 </div>
-        `
-    });
+        `;
+	});
 
-    if (notesObj != 0) {
-        noteWraper.innerHTML = html;
-    } else {
-        noteWraper.innerHTML = '<p>No notes added yet</p>'
-    }
-}
+	if (notesObj != 0) {
+		noteWraper.innerHTML = html;
+	} else {
+		noteWraper.innerHTML = "<p>No notes added yet</p>";
+	}
+};
 
-// View more 
+// View more
 const viewMore = (index) => {
-    let notes = localStorage.getItem('notes');
-    const modalText = document.getElementById('modal_text');
-    if (notes === null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    };
-    modalText.innerText = notesObj[index].input
-    modalContainer.classList.add('show');
-}
+	let notes = localStorage.getItem("notes");
+	const modalText = document.getElementById("modal_text");
+	if (notes === null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
+	modalText.innerText = notesObj[index].input;
+	modalContainer.classList.add("show");
+};
 
-closeButton.addEventListener('click', e => {
-    e.preventDefault();
-    modalContainer.classList.remove('show');
+closeButton.addEventListener("click", (e) => {
+	e.preventDefault();
+	modalContainer.classList.remove("show");
 });
 
 // Remove a note
 const removeNote = (index) => {
-    let notes = localStorage.getItem('notes');
-    if (notes === null) {
-        notesObj = [];
-    } else {
-        notesObj = JSON.parse(notes);
-    };
+	let notes = localStorage.getItem("notes");
+	if (notes === null) {
+		notesObj = [];
+	} else {
+		notesObj = JSON.parse(notes);
+	}
 
-    notesObj.splice(index, 1);
-    localStorage.setItem('notes', JSON.stringify(notesObj));
-    location.reload();
-}
+	notesObj.splice(index, 1);
+	localStorage.setItem("notes", JSON.stringify(notesObj));
+	location.reload();
+};
 
 // Function to truncate the input
 function truncated(input) {
-    if (input.length > 100) {
-        return input.substring(0, 100) + '...';
-    }
-    return input;
-};
+	if (input.length > 100) {
+		return input.substring(0, 100) + "...";
+	}
+	return input;
+}
 
 showNotes();
